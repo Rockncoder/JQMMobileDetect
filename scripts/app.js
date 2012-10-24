b8fd0544d3c7740a2fe98465325144e5473dc1e3
Troy Miles <rockncoder@gmail.com>
2012-10-24 08:38:21 -0700

    Initial commit.

---
+++ scripts/app.js
@@ -0,0 +1,61 @@
+﻿
+var RocknCoder = RocknCoder || {};
+
+(function () {
+	"use strict";
+
+	RocknCoder.Pages = RocknCoder.Pages || {};
+	// put the page events into one string
+	var Events = "pagebeforeshow pageshow pagebeforechange pagechange pagebeforehide pagehide",
+		// the kernel remains unchanged
+		Kernel = function (event) {
+			var that = this,
+				eventType = event.type,
+				pageName = $(this).attr("data-rnc-jspage");
+			if (RocknCoder && RocknCoder.Pages && pageName && RocknCoder.Pages[pageName] && RocknCoder.Pages[pageName][eventType]) {
+				RocknCoder.Pages[pageName][eventType].call(that);
+			}
+		};
+
+	// anonymous function which binds to the page's events
+	(function () {
+		$("div[data-rnc-jspage]").on(
+			Events,
+			Kernel
+		);
+	}());
+
+	// anonymous function which binds to the document's pageload event
+	(function () {
+		$(document).bind(
+			'pageload',
+			function (event, obj) {
+				console.log("pageload");
+				$("div[data-rnc-jspage]")
+					// to make sure we aren't double hooking events clear them all
+					.off(Events)
+					// then hook them all  (the newly loaded page is in DOM at this point)
+					.on(Events, Kernel);
+			}
+		);
+	}());
+
+	// size the content area
+	RocknCoder.Dimensions = (function () {
+		var get = function () {
+			var isFirstPass = false,
+				isIPhone = (/iphone/gi).test(navigator.appVersion),
+				width = $(window).width(),
+				height = $(window).height() + (isIPhone ?  60 : 0),
+				hHeight = $('header').outerHeight(),
+				fHeight = $('footer').outerHeight();
+			return {
+				width: width,
+				height: height - hHeight - fHeight
+			};
+		};
+		return {
+			get: get
+		};
+	}());
+}());
